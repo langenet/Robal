a30f5f5af8c430a00786ea31d881d397ca127eef
alexandroidii <Riccio.peacenlove@gmail.com>
2020-04-05 15:10:58 -0400

    forgot to save product

--- Robal/src/main/java/ac/project/Robal/models/Product.java
+++ Robal/src/main/java/ac/project/Robal/models/Product.java
@@ -1,7 +1,6 @@
 package ac.project.Robal.models;
 
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
