945e3fb86a8f27b8ff7388f2141c06793d83c3f9
alexandroidii <Riccio.peacenlove@gmail.com>
2020-05-04 20:59:32 -0400

    Second attempt for HTTPS and Postgres.  Still broken

--- Robal/src/main/java/ac/project/Robal/RobalApplication.java
+++ Robal/src/main/java/ac/project/Robal/RobalApplication.java
@@ -46,7 +46,7 @@ public class RobalApplication {
 	private Connector redirectConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
-		connector.setPort(8080);
+		connector.setPort(8081);
 		connector.setRedirectPort(8443);
 		return connector;
 	}
