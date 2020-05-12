17250d1518a26907f9c38d7dc7e3d91865e73d31
langenet <robert@lange.ca>
2020-05-11 23:25:20 -0400

    Updated constants, productTest (unfinished)

--- Robal/src/test/java/ac/project/Robal/controllers/ProductControllerTest.java
+++ Robal/src/test/java/ac/project/Robal/controllers/ProductControllerTest.java
@@ -41,7 +41,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -54,14 +53,16 @@ import ac.project.Robal.enums.Constants;
 import ac.project.Robal.models.Account;
 import ac.project.Robal.models.Administrator;
 import ac.project.Robal.models.Customer;
+import ac.project.Robal.models.Product;
 import ac.project.Robal.repositories.AdministratorRepository;
-import ac.project.Robal.repositories.CustomerRepository;
+import ac.project.Robal.repositories.ProductRepository;
 import ac.project.Robal.services.AccountService;
+import ac.project.Robal.services.ProductService;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-public class AdministratorControllerTest2 extends Constants {
+public class ProductControllerTest extends Constants {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -73,7 +74,7 @@ public class AdministratorControllerTest2 extends Constants {
 	private AccountService accountService;
 
 	@Autowired
-	private AdministratorRepository administratorRepository;
+	private ProductRepository productRepository;
 
 	@MockBean
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -91,28 +92,31 @@ public class AdministratorControllerTest2 extends Constants {
 	}
 
 	@Test
-	void createAdministrator() throws Exception {
+	void createProduct() throws Exception {
 		Administrator admin = accountService.saveAdministrator(getAdmin1());
-		int databaseSizeBeforeCreate = administratorRepository.findAll().size();
+		int databaseSizeBeforeCreate = productRepository.findAll().size();
+	
+		productRepository.save(getProduct1());
 
 		this.mockMvc
-				.perform(post("/admins/").contentType(TestUtil.APPLICATION_JSON_UTF8)
+				.perform(post("/products/").contentType(TestUtil.APPLICATION_JSON_UTF8)
 						.headers(TestUtil.getAuthorizationBasic(getAdmin1().getEmail(), getAdmin1().getPassword()))
 						.content(TestUtil.convertObjectToJsonBytes(getAdmin2())))
-				.andExpect(status().isCreated()).andExpect(jsonPath("$.accountId").isNumber())
-				.andExpect(jsonPath("$.name").value(NAME2)).andExpect(jsonPath("$.email").value(EMAIL_ADMIN2))
-				.andExpect(jsonPath("$.role").value(ADMIN_ROLE.name()));
-	
-		List<Administrator> administrator = administratorRepository.findAll();
-		assertThat(administrator.size()).isEqualTo(databaseSizeBeforeCreate + 1);
+				.andExpect(status().isCreated()).andExpect(jsonPath("$.productId").isNumber())
+				.andExpect(jsonPath("$.discription").value(PRO_DESC1))
+				.andExpect(jsonPath("$.name").value(PRO_NAME1))
+				.andExpect(jsonPath("$.sku").value(PRO_SKU1));
+		
+		List<Product> products = productRepository.findAll();
+		assertThat(products.size()).isEqualTo(databaseSizeBeforeCreate + 1);
 	}
 
 	@Test
-	void findAdministrator() throws Exception {
+	void findProduct() throws Exception {
 
 		Administrator admin = accountService.saveAdministrator(getAdmin1());
 		
-		this.mockMvc.perform(get("/admins/{id}", admin.getAccountId())
+		this.mockMvc.perform(get("/products/{id}", admin.getAccountId())
 				// Pass in the header
 						.headers(TestUtil.getAuthorizationBasic(getAdmin1().getEmail(), getAdmin1().getPassword())))
 				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -124,38 +128,38 @@ public class AdministratorControllerTest2 extends Constants {
 	}
 	
 	@Test
-	void DeleteAdministrator() throws Exception {
+	void DeleteProduct() throws Exception {
 		
 		Account admin = accountService.saveAdministrator(getAdmin1());
-		int databaseSizeBeforeDelete = administratorRepository.findAll().size();
+		int databaseSizeBeforeDelete = productRepository.findAll().size();
 	
-		this.mockMvc.perform(delete("/admins/{id}", admin.getAccountId())
+		this.mockMvc.perform(delete("/products/{id}", admin.getAccountId())
 				// Pass in the header
 						.headers(TestUtil.getAuthorizationBasic(getAdmin1().getEmail(), getAdmin1().getPassword())))
 				.andExpect(status().isOk())				
 				.andReturn();
 				
 				// Validate the database is empty
-				List<Administrator> accounts = administratorRepository.findAll();
-				assertThat(accounts.size()).isEqualTo(databaseSizeBeforeDelete - 1);
+				List<Product> product = productRepository.findAll();
+				assertThat(product.size()).isEqualTo(databaseSizeBeforeDelete - 1);
 		
 	}
 
 	
 	
 	@Test
-	void findAdministrators() throws Exception {
+	void findProducts() throws Exception {
 
-		List<Customer> customers = new ArrayList<>();
+		List<Product> products = new ArrayList<>();
 
-		accountService.saveCustomer(getCustomer1());
-		customers.add(getCustomer1());
+		productRepository.save(getProduct1());
+		products.add(getProduct1());
 
-		accountService.saveCustomer(getCustomer2());
-		customers.add(getCustomer2());
+		productRepository.save(getProduct2());
+		products.add(getProduct2());
 
-		accountService.saveCustomer(getCustomer3());
-		customers.add(getCustomer3());
+		productRepository.save(getProduct2());
+		products.add(getProduct2());
 
 		accountService.saveAdministrator(getAdmin1());
 
@@ -172,41 +176,40 @@ public class AdministratorControllerTest2 extends Constants {
 				new TypeReference<List<Customer>>() {
 				});
 
-		assertThat(actual.equals(customers));
+		assertThat(actual.equals(products));
 
 	}
 	
-	@Test
-	void updateAccountName() throws Exception {
-		
-		Account initial = accountService.saveAdministrator(getAdmin2());
-		Account admin = accountService.saveAdministrator(getAdmin1());
-		
-		int databaseSizeBeforeUpdate = administratorRepository.findAll().size();
-
-		Account updated = administratorRepository.findById(initial.getAccountId()).orElse(null);
-		assertThat(updated).isNotNull();
-		entityManager.detach(updated);
-		
-		updated.setName(UPDATED_NAME);
-
-		
-		mockMvc.perform(put("/admins/{id}/name", initial.getAccountId())
-				.contentType(TestUtil.APPLICATION_JSON_UTF8)
-				.content(TestUtil.convertObjectToJsonBytes(updated))
-				.headers(TestUtil.getAuthorizationBasic(getAdmin1().getEmail(), getAdmin1().getPassword())))
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("$.accountId").value(initial.getAccountId()))
-				.andExpect(jsonPath("$.name").value(updated.getName())).andReturn();
-
-
-
-		List<Administrator> adminAccounts = administratorRepository.findAll();
-		assertThat(adminAccounts.size()).isEqualTo(databaseSizeBeforeUpdate);
-		// It is also helpful to verify that the database has indeed changed
-		Account databaseAccount = administratorRepository.findById(initial.getAccountId()).orElse(null);
-		assertThat(databaseAccount).isNotNull();
-		assertThat(databaseAccount.getName()).isEqualTo(updated.getName());
-
-	}
+//	@Test
+//	void updateProduct() throws Exception {
+//	
+//		Account admin = accountService.saveAdministrator(getAdmin1());
+//		
+//		int databaseSizeBeforeUpdate = productRepository.findAll().size();
+//
+//		Product updated = productRepository.findById(initial.getAccountId()).orElse(null);
+//		assertThat(updated).isNotNull();
+//		entityManager.detach(updated);
+//		
+//		updated.setName(UPDATED_NAME);
+//
+//		
+//		mockMvc.perform(put("/admins/{id}/name", initial.getAccountId())
+//				.contentType(TestUtil.APPLICATION_JSON_UTF8)
+//				.content(TestUtil.convertObjectToJsonBytes(updated))
+//				.headers(TestUtil.getAuthorizationBasic(getAdmin1().getEmail(), getAdmin1().getPassword())))
+//				.andExpect(status().isCreated())
+//				.andExpect(jsonPath("$.accountId").value(initial.getAccountId()))
+//				.andExpect(jsonPath("$.name").value(updated.getName())).andReturn();
+//
+//
+//
+//		List<Administrator> adminAccounts = administratorRepository.findAll();
+//		assertThat(adminAccounts.size()).isEqualTo(databaseSizeBeforeUpdate);
+//		// It is also helpful to verify that the database has indeed changed
+//		Account databaseAccount = administratorRepository.findById(initial.getAccountId()).orElse(null);
+//		assertThat(databaseAccount).isNotNull();
+//		assertThat(databaseAccount.getName()).isEqualTo(updated.getName());
+//
+//	}
 }
